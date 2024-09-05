dcab5dd5186517a3e9785aa26a12d8e46ea95412
GabrielGoulart <gabriel.gomesgoulart@gmail.com>
2024-09-05 16:17:12 -0300

    Implementado o Desafio da DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Mokia");
+
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Nokia", imei: "123456", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Google Maps");
+
+Console.WriteLine("-------------------------------");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "789123", modelo: "Iphone", imei: "789123", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WAZE");
